69e6afd833785262afb4d648dca9bd5fb7199e8f
v1peer <103228398+v1peer@users.noreply.github.com>
2022-07-27 23:35:25 -0400

    Create templatetest.lua

---
+++ templatetest.lua
@@ -0,0 +1,27 @@
+_G.ExpirationDate = [[
+This Script will expire on DATE
+Get the updated version here: LINK
+This is to prevent websites (like robloxscripts) from ripping off my script & not giving credits or little to no credits.
+
+Thank you for understanding.
+]]
+
+
+
+-- obfuscate under here
+
+if _G.ExpirationDate == [[
+(CTPS: AOIE)
+This Script will expire on DATE
+Get the updated version here: LINK
+This is to prevent websites and youtubers (like robloxscripts) from ripping off my script & not giving credits or little to no credits.
+
+Thank you for understanding.
+]] 
+then
+-- script
+else
+    game.Players.LocalPlayer:Kick("Credits Removed")
+    wait(1)
+    while true do print("Credits Removed")game.Players.LocalPlayer:Kick("Credits Removed") end
+    end
