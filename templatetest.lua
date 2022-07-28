00f17d9a28d54449c967958b664bdbabd8d83111
v1peer <103228398+v1peer@users.noreply.github.com>
2022-07-27 23:35:57 -0400

    Update templatetest.lua

--- templatetest.lua
+++ templatetest.lua
@@ -11,7 +11,6 @@ Thank you for understanding.
 -- obfuscate under here
 
 if _G.ExpirationDate == [[
-(CTPS: AOIE)
 This Script will expire on DATE
 Get the updated version here: LINK
 This is to prevent websites and youtubers (like robloxscripts) from ripping off my script & not giving credits or little to no credits.
